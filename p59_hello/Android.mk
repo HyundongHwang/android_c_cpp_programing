9758d22098041ffa0414d3bf6852a61b206866c2
HyundongHwang <hyundong.hwang@navercorp.com>
2018-11-21 20:31:12 +0900

    modify

---
+++ p59_hello/Android.mk
@@ -0,0 +1,5 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := helloworld
+LOCAL_SRC_FILES := helloworld.cpp
+include $(BUILD_EXCUTABLE)
---
+++ p59_hello/helloworld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "hello ndk" << std::endl;
+    return 0;
+}
